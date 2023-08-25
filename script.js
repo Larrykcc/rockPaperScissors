827b5d3b186b716216b4c4931499089e7fd8656f
Lawrence Tokar <10758905+Larrykcc@users.noreply.github.com>
2023-08-25 09:16:21 +0700

    just checking to see this works

--- script.js
+++ script.js
@@ -2,4 +2,13 @@
 
 function getComputerChoice () {
     //Math.floor(Math.random()*10) this will create integer and round it.
+        const int = Math.floor(Math.random()*10)
+        if(int >= 1 && int <=3) {
+            console.log('rock')
+        } else if (int >=4 &&  int <=6) {
+            console.log('paper')
+        } else {
+            console.log('scissors')
+        }
+
 }
